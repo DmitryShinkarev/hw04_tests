a92e4621623a21b3c8e75fa031f4290df0ec7022
Dmitriy Shinkarev <dsh.dev33@gmail.com>
2020-04-20 22:30:57 +0300

    first check

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.utils import timezone
 from .models import Post, Group, User
 from .forms import PostForm
 
